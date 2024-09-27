4d874841c9a6e4548242fe6c66bfbd900a67fd22
m1st0rm <mishaboy333@gmail.com>
2024-09-27 10:55:53 +0300

    Added task_3.sql and updated task_1.sql

--- db-queries/task_1.sql
+++ db-queries/task_1.sql
@@ -1,4 +1,5 @@
 -- Вывести количество фильмов в каждой категории, отсортировать по убыванию.
+
 select category.name, count(*) as films_in_category_count
 from category
 inner join film_category on category.category_id = film_category.category_id
